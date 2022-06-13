5c0df03c421c5519ee7fd399fde5efd3d07236fd
Tidbits JS <tidbitsjs@gmail.com>
2022-06-13 23:53:10 +0530

    navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { logo } from "../assets";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <Link to="/" className="home-link">
+          <img src={logo} alt="jwt_auth" className="logo" />
+        </Link>
+        <div className="nav-links">
+          <p>hello, Tidbits</p>
+          <button className="btn btn-small">logout</button>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.nav`
+  background: var(--white);
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .nav-center {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  .nav-links {
+    display: flex;
+    flex-direction: column;
+  }
+  .nav-links p {
+    margin: 0;
+    text-transform: capitalize;
+    margin-bottom: 0.25rem;
+  }
+  .home-link {
+    display: flex;
+    align-items: flex-end;
+  }
+  @media (min-width: 776px) {
+    .nav-links {
+      flex-direction: row;
+      align-items: center;
+    }
+    .nav-links p {
+      margin: 0;
+      margin-right: 1.5rem;
+    }
+  } ;
+`;
+
+export default Navbar;
