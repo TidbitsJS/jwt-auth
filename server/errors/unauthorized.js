bf741f5e28719e3ff06a5a674c7115266b77d385
Tidbits JS <tidbitsjs@gmail.com>
2022-07-03 23:53:45 +0530

    jwt authorization

--- server/errors/unauthorized.js
+++ server/errors/unauthorized.js
@@ -1,11 +1,11 @@
 const { statusCode } = require("http-status-codes");
 const CustomAPIError = require("./custom-api");
 
-class UnauthenticatedError extends CustomAPIError {
+class UnauthorizedError extends CustomAPIError {
   constructor(message) {
     super(message);
     this.statusCode = statusCode.FORBIDDEN;
   }
 }
 
-module.exports = UnauthenticatedError;
+module.exports = UnauthorizedError;
---
+++ server/middleware/full-auth.js
@@ -0,0 +1,44 @@
+const CustomError = require("../errors");
+const { isTokenVaild } = require("../utils/jwt");
+
+const authenticateUser = async (req, res, next) => {
+  let token;
+
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+
+  try {
+    const payload = isTokenVaild(token);
+
+    req.user = {
+      userId: payload.user.userId,
+      role: payload.user.role,
+    };
+
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Unathorized to access this route"
+      );
+    }
+
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
