f12c0c23457f337d9e32587b5263a1d108a518ac
Tidbits JS <tidbitsjs@gmail.com>
2022-07-08 23:57:12 +0530

    user permissions

---
+++ server/utils/checkPermissions.js
@@ -0,0 +1,9 @@
+const CustomError = require("../errors");
+
+const checkPermissions = (requestUser, resourceUserId) => {
+  if (requestUser.role === "admin") return;
+  if (requestUser.userId === resourceUserId.toString()) return;
+  throw new CustomError.UnauthorizedError("Unauthorized to access this route");
+};
+
+module.exports = checkPermissions;
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,6 +1,7 @@
 const { createJWT, isTokenVaild, attachCookiesToResponse } = require("./jwt");
 const createTokenUser = require("./createTokenUser");
 const createHash = require("./createHash");
+const checkPermissions = require("./checkPermissions");
 
 module.exports = {
   createJWT,
@@ -8,4 +9,5 @@ module.exports = {
   attachCookiesToResponse,
   createTokenUser,
   createHash,
+  checkPermissions,
 };
