7a230ae0600a17b2a0d69a0e309b1575c9831a59
Tidbits JS <tidbitsjs@gmail.com>
2022-07-11 23:44:30 +0530

    register user auth

---
+++ server/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const Token = require("../models/Token");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const {
+  attachCookiesToResponse,
+  createTokenUser,
+  sendVerificationEmail,
+} = require("../utils");
+
+const crypto = require("crypto");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const verificationToken = crypto.randomBytes(40).toString("hex");
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken,
+  });
+
+  const origin = `${req.protocol}://${req.get("host")}`;
+
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    verificationToken: user.verificationToken,
+    origin,
+  });
+
+  res.status(StatusCodes.CREATED).json({
+    msg: "Success! Please check your email to verify account",
+  });
+};
+
+module.exports = {
+  register,
+};
