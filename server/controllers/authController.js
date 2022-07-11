53bd577be16adf25f0c9495abce5c84371fdb6f5
Tidbits JS <tidbitsjs@gmail.com>
2022-07-11 23:53:36 +0530

    user login logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -46,6 +46,73 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  if (!user.isVerified) {
+    throw new CustomError.UnauthenticatedError("Please verify your account");
+  }
+
+  const tokenUser = createTokenUser(user);
+  let refreshToken = "";
+  const existingToken = await Token.findOne({ user: user._id });
+
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError("Invalid Credentials");
+    }
+
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
+  }
+
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  await Token.create(userToken);
+  attachCookiesToResponse({ res, user: userToken, refreshToken });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
+const logout = async (req, res) => {
+  await Token.findOneAndDelete({ user: req.user.userId });
+
+  res.cookie("accessToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.cookie("refreshToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+};
+
 module.exports = {
   register,
+  login,
+  logout,
 };
