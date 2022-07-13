2c01bfcd9ef82d0a7a6207110b0569f94a7adefb
Tidbits JS <tidbitsjs@gmail.com>
2022-07-13 21:59:38 +0530

    auth & user routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const { authenticateUser } = require("../middleware/authentication");
+const {
+  register,
+  login,
+  logout,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", authenticateUser, logout);
+router.post("/verify-email", verifyEmail);
+router.post("/reset-password", resetPassword);
+router.post("/forgot-password", forgotPassword);
+
+module.exports = router;
