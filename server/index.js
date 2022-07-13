4933a3117ddebbe3dda46662e5d9073597e5ead7
Tidbits JS <tidbitsjs@gmail.com>
2022-07-13 23:32:28 +0530

    index routes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+require("dotenv").config();
+require("express-async-errors");
 
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const connectDB = require("./db/connect");
+
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-const userRoutes = require("./routes");
+app.set("trust proxy", 1);
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
 
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
-app.use(userRoutes);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
