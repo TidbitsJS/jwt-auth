e1acf2ee3e35ee00e1110921ea98030bf3615f2d
Tidbits JS <tidbitsjs@gmail.com>
2022-07-15 23:21:57 +0530

    app routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import { Navbar } from "./components";
+import {
+  Dashboard,
+  ForgotPassword,
+  Home,
+  Login,
+  Error,
+  Register,
+  ResetPassword,
+} from "./pages";
+import { useGlobalContext } from "./context";
+
+const App = () => {
+  const { isLoading } = useGlobalContext();
+
+  if (isLoading) {
+    return (
+      <section className="page page-center">
+        <div className="loading" />
+      </section>
+    );
+  }
+
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/user/reset-password" element={<ResetPassword />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as ResetPassword } from "./ResetPassword";
 export { default as ForgotPassword } from "./ForgotPassword";
 export { default as Dashboard } from "./Dashboard";
 export { default as Home } from "./Home";
+export { default as Register } from "./Register";
