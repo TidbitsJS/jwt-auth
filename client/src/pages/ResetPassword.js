180d21efcc6ea712ed3259f4b7598533690c0a9c
Tidbits JS <tidbitsjs@gmail.com>
2022-07-18 23:23:27 +0530

    reset password api integration

--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,33 +1,87 @@
 import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 import { FormRow } from "../components";
+import { useLocalState } from "../utils";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+  const query = useQuery();
   const [password, setPassword] = useState("");
+
+  const { alert, showAlert, loading, setLoading, success, setSuccess } =
+    useLocalState();
 
   const handleChange = async (e) => {
     setPassword(e.target.value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!password) {
+      showAlert({ text: "Please enter password" });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const { data } = await axios.post("/api/v1/auth/reset-password", {
+        password,
+        token: query.get("token"),
+        email: query.get("email"),
+      });
+
+      console.log(data);
+
+      setSuccess(true);
+      showAlert({
+        text: "Success, redirecting to login page shortly",
+        type: "success",
+      });
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
+    } catch (error) {
+      showAlert({ text: error.response.data.msg });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Wrapper className="page">
-      <div className="alert">Alert</div>
+      {alert.show && (
+        <div className={`alert alert-${alert.type}`}>{alert.text}</div>
+      )}
 
-      <form className="form">
-        <h4>Reset Password</h4>
+      {!success && (
+        <form
+          className={loading ? "form form-loading" : "form"}
+          onSubmit={handleSubmit}
+        >
+          <h4>Reset Password</h4>
 
-        <FormRow
-          type="password"
-          name="password"
-          value={password}
-          handleChange={handleChange}
-        />
+          <FormRow
+            type="password"
+            name="password"
+            value={password}
+            handleChange={handleChange}
+          />
 
-        <button type="submit" className="btn btn-block">
-          New Password
-        </button>
-      </form>
+          <button type="submit" className="btn btn-block" disabled={loading}>
+            {loading ? "Please Wait..." : "New Password"}
+          </button>
+        </form>
+      )}
     </Wrapper>
   );
 };
