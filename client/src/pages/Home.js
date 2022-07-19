2fa6c58cf751d0b7d95d67aa49d08f6a016f218a
Tidbits JS <tidbitsjs@gmail.com>
2022-07-19 23:46:55 +0530

    home page styling changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,31 +13,37 @@ const Home = () => {
       {user && <Navigate to="/dashboard" />}
       <Wrapper className="page">
         <div className="info">
-          <h2>
-            <span>Auth</span> Workflow
-          </h2>
+          <div className="info_text">
+            <h2>
+              <span>Auth</span> Workflow
+            </h2>
 
-          <p>
-            I'm baby viral enamel pin chartreuse cliche retro af selfies kinfolk
-            photo booth plaid jianbing actually squid 3 wolf moon lumbersexual.
-            Hell of humblebrag gluten-free lo-fi man braid legging
-          </p>
+            <p>
+              I'm baby viral enamel pin chartreuse cliche retro af selfies
+              kinfolk photo booth plaid jianbing actually squid 3 wolf moon
+              lumbersexual. Hell of humblebrag gluten-free lo-fi man braid
+              legging
+            </p>
 
-          <p>
-            Cloud bread kale chips wayfarers deep v chicharrones leggings
-            fingerstache actually blog cliche four dollar toast. Sriracha ugh
-            kickstarter, next level la croix butcher lomo.
-          </p>
+            <p>
+              Cloud bread kale chips wayfarers deep v chicharrones leggings
+              fingerstache actually blog cliche four dollar toast. Sriracha ugh
+              kickstarter, next level la croix butcher lomo.
+            </p>
 
-          <Link to="/login" className="btn">
-            Login
-          </Link>
+            <div className="info_btns">
+              <Link to="/login" className="btn">
+                Login
+              </Link>
 
-          <Link to="/register" className="btn">
-            Register
-          </Link>
-
-          <img src={main} alt="job hunt" className="img main-img" />
+              <Link to="/register" className="btn">
+                Register
+              </Link>
+            </div>
+          </div>
+          <div className="info_img">
+            <img src={main} alt="job hunt" className="img main-img" />
+          </div>
         </div>
       </Wrapper>
     </>
@@ -45,27 +51,54 @@ const Home = () => {
 };
 
 const Wrapper = styled.div`
-  display: grid;
+  display: flex;
   align-items: center;
+  justify-content: center;
+  .info {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
   h2 {
     font-weight: 700;
   }
   h2 span {
     color: var(--primary-500);
   }
-  .main-img {
-    display: none;
+  .info_text {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-    column-gap: 6rem;
-    .main-img {
-      display: block;
-    }
+  .info_img {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 10px;
+  }
+  .main-img {
+    display: flex;
+    width: 80%;
+    height: 80%;
+  }
+  .info_btns {
+    display: flex;
+    flex-direction: row;
   }
   .btn {
     margin-left: 0.25rem;
     margin-right: 0.25rem;
+  }
+
+  @media (max-width: 900px) {
+    .info {
+      flex-direction: column;
+    }
+    .info_img {
+      margin-left: 0px;
+      margin-top: 30px;
+    }
   }
 `;
 
